986b5fe1d43a41b1735335a4a6e7b91fa7a2cfcb
varun <varunkumar2004.vk@gmail.com>
2025-11-10 23:06:01 +0530

    Closed Testing Application version 2.

--- app/src/main/java/com/varunkumar/wayfinder/core/presentation/AppBottomBar.kt
+++ app/src/main/java/com/varunkumar/wayfinder/core/presentation/AppBottomBar.kt
@@ -1,19 +1,10 @@
 package com.varunkumar.wayfinder.core.presentation
 
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Icon
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun AppBottomBar(
